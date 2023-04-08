57efe82c3fd43ce1f5fcfad1b802f88a845c9cd6
Gabi Nguyen <100677997+GabiNguyen@users.noreply.github.com>
2023-04-08 18:05:33 +0200

    feat 4: fixed the bugs for playing fetch

---
+++ Prototype2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float elapsedTime;
+    private float coolDown = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if the total elapsed time is greater than the cooldown, the player is allowed to send a dog
+        if (elapsedTime > coolDown)
+        {
+            // On spacebar press, send dog
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                // reset the elapsed time
+                elapsedTime = 0.0f;
+            }
+        } 
+        // if the total elapsed time is not enough to send a dog, the elapsed time is increased
+        else {
+            elapsedTime += Time.deltaTime;
+        }
+    }
+}
